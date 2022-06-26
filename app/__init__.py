6b47ff31ddcdf1a696c6566f4197164a87dc0a32
emilyirenelai <88216671+emilyirenelai@users.noreply.github.com>
2022-06-25 22:18:38 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,17 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
+
+mydb =
+MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+              user=os.getenv("MYSQL_USER"),
+              password=os.getenv("MYSQL_PASSWORD"),
+              host=os.getenv("MYSQL_HOST"),
+              port=3306
+             )
+
+print(db)
+
 app.register_blueprint(map_app)
 dataFile = open("app\static\data.json" , encoding = "utf-8")
 
