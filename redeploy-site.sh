df278d6c8e71020af58fa217a35380366efc4986
emilyirenelai <emilyirenelai@gmail.com>
2022-07-20 12:56:53 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,8 +6,8 @@ git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
 
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-RUN="flask run --host=0.0.0.0"
-systemctl daemon-reload
-systemctl restart myportfolio
+#source python3-virtualenv/bin/activate
+#pip install -r requirements.txt
+#RUN="flask run --host=0.0.0.0"
+#systemctl daemon-reload
+#systemctl restart myportfolio
